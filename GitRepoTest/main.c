c2e9c7bad4e52c611592f3372544a6d370800c95
Florin Olteanu <florryinn@yahoo.com>
2018-04-06 10:42:37 +0300

    Modificare main

--- GitRepoTest/main.c
+++ GitRepoTest/main.c
@@ -8,9 +8,16 @@
 #include <stdio.h>
 #include "functii.h"
 
+
 int main()
 {
+	int a=0;
+	while(a<5)
+	{
+		a++;
+	}
 	printf("Hello World!\n");
 	printf("Suma numere 2 + 3: %d",sumaNumere(2,3));
+	printf("\nHello word!\n");
 	return 0;
 }
